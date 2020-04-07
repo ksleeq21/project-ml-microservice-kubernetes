f1a992c8bb6bfb8a18dfdb4cb5b763174ea5be10
Kangsan Lee <klee@Kangsans-MacBook-Pro.local>
2020-04-06 22:09:16 -0700

    Update log file and update docker script files

--- run_docker.sh
+++ run_docker.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
-## Complete the following steps to get Docker running locally
-
-# Step 1:
 # Build image and add a descriptive tag
+docker build --tag=ksleeq21/ml-us-k8 .
 
-# Step 2: 
 # List docker images
+docker image ls 
 
-# Step 3: 
 # Run flask app
+docker run -p 8000:80 ksleeq21/ml-us-k8
--- upload_docker.sh
+++ upload_docker.sh
@@ -3,13 +3,11 @@
 
 # Assumes that an image is built via `run_docker.sh`
 
-# Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=ml-us-k8
 
-# Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 
-# Step 3:
 # Push image to a docker repository
+docker push ksleeq21/ml-us-k8:latest
